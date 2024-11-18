86ebc35651d8e72522298b40463f7cf3720370c7
Avinash Kumar Gupta <robozealous@gmail.com>
2024-11-18 15:27:21 +0530

    second commit for initial setup

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+
+const app = express();
+const PORT = process.env.PORT || 8080
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log("Listening on port", PORT)
+})
